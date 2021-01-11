e5304875958e2e4d95cda9d1e9888bf300c679bc
peng49 <1219955253@qq.com>
2021-01-11 11:52:21 +0800

    客户端应用

--- dubbo-two/dubbo-two-server/src/main/java/com/github/peng49/dubbotwo/server/BootApplication.java
+++ dubbo-two/dubbo-two-server/src/main/java/com/github/peng49/dubbotwo/server/BootApplication.java
@@ -4,10 +4,32 @@ import com.github.peng49.dubboone.api.service.HelloService;
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.bootstrap.DubboBootstrap;
 import org.apache.dubbo.config.utils.ReferenceConfigCache;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+
+@EnableAutoConfiguration
 public class BootApplication {
+
+    @DubboReference(url = "zookeeper://localhost:2181")
+    private HelloService helloService;
+
+    @Bean
+    public ApplicationRunner test()
+    {
+        return args -> {
+            System.out.println("test");
+        };
+    }
+
+
     public static void main(String[] args) {
         /*ReferenceConfig<HelloService> reference = new ReferenceConfig<>();
         reference.setApplication(new ApplicationConfig("dubbo-auto-configuration-provider-demo"));
@@ -16,7 +38,7 @@ public class BootApplication {
         HelloService service = reference.get();
         String message = service.say();*/
 
-        ReferenceConfig<HelloService> reference = new ReferenceConfig<>();
+        /*ReferenceConfig<HelloService> reference = new ReferenceConfig<>();
         reference.setInterface(HelloService.class);
         reference.setGeneric("true");
 
@@ -31,6 +53,7 @@ public class BootApplication {
 
         System.out.println(message);
 
-        System.out.println(service.say("good"));
+        System.out.println(service.say("good"));*/
+        SpringApplication.run(BootApplication.class).close();
     }
 }
